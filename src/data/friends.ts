9a5c04e38efecf1e1b07c1eb3ab0e69257993698
Li Yan <liyanqwq@gmail.com>
2025-05-23 07:25:04 +0800

    Update friends.ts

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -1,11 +1,11 @@
 export const friends = [
-    {
-        "name": "NuttyShell",
-        "url": "https://polyuctf.com",
-        "avatar": "https://polyuctf.com/wp-content/uploads/2023/07/cropped-Nuttyshell_Logo_Finalized-corp-square.png",
-        "description": "PolyU CTF Team",
-        "rounded": 'lg'
-    },
+    // {
+    //     "name": "NuttyShell",
+    //     "url": "https://polyuctf.com",
+    //     "avatar": "https://polyuctf.com/wp-content/uploads/2023/07/cropped-Nuttyshell_Logo_Finalized-corp-square.png",
+    //     "description": "PolyU CTF Team",
+    //     "rounded": 'lg'
+    // },
     {
         "name": "Shiro",
         "url": "https://shiro.love",
