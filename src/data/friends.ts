0f047779b9bfc0cd49df25a574cc174ec20d7f30
Li Yan <liyanqwq@gmail.com>
2025-06-17 23:31:52 +0800

    Fix UI

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -9,9 +9,23 @@ export const friends = [
     {
         "name": "Shiro",
         "url": "https://shiro.love",
-        "avatar": "https://assets.moedev.cn/blog/photo/user/head.jpg!webp",
+        "avatar": "/assets/friends/shiro.webp",
         "description": "夙兴夜，勤不怠。",
         "rounded": ''
     },
-    
+    // 網站沒有建好。
+    // {
+    //     "name": "Syrup",
+    //     "url": "https://deluxesyrup.github.io/",
+    //     "avatar": "/assets/friends/syrupchan.webp",
+    //     "description": "",
+    //     "rounded": ''
+    // },
+    {
+        "name": "風凪的小窝",
+        "url": "https://blog.bh2vsq.cn/",
+        "avatar": "https://blog.bh2vsq.cn/photo/avatar/avatar.jpg",
+        "description": "DE BH2VSQ 喵~",
+        "rounded": ''
+    }
 ];
